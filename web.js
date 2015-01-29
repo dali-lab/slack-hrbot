423b427e217dab5475c6df05904ff72d46cfdb4c
Nook <nook@harquail.com>
2015-01-29 18:54:13 -0500

    if board name not in mapping to users, use board name in assignment message, rather than undefined

--- web.js
+++ web.js
@@ -15,8 +15,8 @@ var https = require('https');
 app.use(logfmt.requestLogger());
 
 var token = process.env.SLACK_BOT_TOKEN, // Add a bot at https://my.slack.com/services/new/bot and copy the token here.
-  autoReconnect = true,
-  autoMark = true;
+autoReconnect = true,
+autoMark = true;
 
 var slack = new Slack(token, autoReconnect, autoMark);
 var lastUrgentMessageTime = 0.0;
@@ -24,9 +24,9 @@ var lastUrgentMessageTime = 0.0;
 slack.on('open', function() {
 
   var channels = [],
-    groups = [],
-    unreads = slack.getUnreadCount(),
-    key;
+  groups = [],
+  unreads = slack.getUnreadCount(),
+  key;
 
   for (key in slack.channels) {
     if (slack.channels[key].is_member) {
@@ -49,11 +49,11 @@ slack.on('open', function() {
 slack.on('message', function(message) {
 
   var type = message.type,
-    channel = slack.getChannelGroupOrDMByID(message.channel),
-    user = slack.getUserByID(message.user),
-    time = message.ts,
-    text = message.text,
-    response = '';
+  channel = slack.getChannelGroupOrDMByID(message.channel),
+  user = slack.getUserByID(message.user),
+  time = message.ts,
+  text = message.text,
+  response = '';
 
   //if the message is in urgent-important
   if (type === 'message' && channel.name === 'urgent-important') {
@@ -63,103 +63,100 @@ slack.on('message', function(message) {
     //only flash lights if it's been 90 seconds since the last message
     if (message.ts - lastUrgentMessageTime > 90) {
       request({
-          url: "http://dali-lights.herokuapp.com",
-          method: "POST",
-          json: {
-            text: "pulse"
-          },
+        url: "http://dali-lights.herokuapp.com",
+        method: "POST",
+        json: {
+          text: "pulse"
         },
-        function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-            console.log(body)
-          }
+      },
+      function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body)
         }
-      );
+      }
+    );
 
-      console.log('!!!message in todos!!!');
-    }
-    lastUrgentMessageTime = parseFloat(time);
+    console.log('!!!message in todos!!!');
   }
+  lastUrgentMessageTime = parseFloat(time);
+}
 });
 
 slack.on('error', function(error) {
 
-  console.error('Error: %s', error);
+console.error('Error: %s', error);
 });
 
 slack.login();
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+res.send('Hello World!');
 });
 
 app.get('/trello-webhook', function(req, res) {
-  res.send('Hello Trello!');
+res.send('Hello Trello!');
 
 
-  console.log(req);
+console.log(req);
 
 });
 
 //called by trello when cards are created
 app.post('/trello-webhook', function(req, res) {
-  res.send('Hello World!');
-  console.log('REQUEST POSTED\n' + JSON.stringify(req.body));
+res.send('Hello World!');
+console.log('REQUEST POSTED\n' + JSON.stringify(req.body));
   
-  //the kind of thing that was done
-  var actionType = req.body.action.type;
-  //description of thing
-  var brief =  req.body.action.data.card.name;
+//the kind of thing that was done
+var actionType = req.body.action.type;
+//description of thing
+var brief =  req.body.action.data.card.name;
   
   
-  //mapping of trello boards to slack users
-  var boardsAndPeople = {'Technical Director': '<@tim>','Administrations': '<@kaitlin>','Lead Designer': '<@alisonleung>','Marketing': '<@sofia>','Project Management':'<@sean> & <@tim_serkes>','Digital Arts Apprentice': '<@mattstanton>','Mentor Tasks': 'mentors'};
-         
-  var TrelloNamesAndPeople = {'Tim Tregubov': '<@tim>','Lorie Loeb': '<@lorie>','Sean Oh': '<@sean>','Kaitlin Maier': '<@kaitlin>','Alison Leung': '<@alisonleung>','Sofia Rainaldi': '<@sofia>','Tim Serkes': '<@tim_serkes>','Matt Stanton': '<@mattstanton>','Nook Harquail': '<@nook>','Marissa Allen': '<@marissa>','Runi Goswami': '<@runi>','Mentor Tasks': 'mentors'};
+//mapping of trello boards to slack users
+var boardsAndPeople = {'Technical Director': '<@tim>','Administrations': '<@kaitlin>','Lead Designer': '<@alisonleung>','Marketing': '<@sofia>','Project Management':'<@sean> & <@tim_serkes>','Digital Arts Apprentice': '<@mattstanton>','Mentor Tasks': 'mentors'};
+//mapping of trello usernames to slack usernames         
+var TrelloNamesAndPeople = {'Tim Tregubov': '<@tim>','Lorie Loeb': '<@lorie>','Sean Oh': '<@sean>','Kaitlin Maier': '<@kaitlin>','Alison Leung': '<@alisonleung>','Sofia Rainaldi': '<@sofia>','Tim Serkes': '<@tim_serkes>','Matt Stanton': '<@mattstanton>','Nook Harquail': '<@nook>','Marissa Allen': '<@marissa>','Runi Goswami': '<@runi>','Mentor Tasks': 'mentors'};
 
-  //the name of the assigner
-  var assigner = req.body.action.memberCreator.fullName;
-  //link to the card
-  var linky = req.body.action.data.card.shortLink;
+//the name of the assigner
+var assigner = req.body.action.memberCreator.fullName;
+//link to the card
+var linky = req.body.action.data.card.shortLink;
   
-  var staffGroup = slack.getGroupByName('staff');
-  //a new card was created
-  if(actionType == 'createCard'){
-    //board the thing was posted on
-    var boardAssignedTo = req.body.action.data.list.name;
+var staffGroup = slack.getGroupByName('staff');
+//a new card was created
+if(actionType == 'createCard'){
+  //board the thing was posted on
+  var boardAssignedTo = req.body.action.data.list.name;
     
-    function taskassignedToBoard(board) {
-      //person the thing was assigned to
-      var asignee = boardsAndPeople[board];
-      var response = '_' + brief + '_ ' + 'assigned to ' + asignee + ' \nhttp://trello.com/c/'+ linky;
-      staffGroup.send(response);
+  function taskassignedToBoard(board) {
+    //person the thing was assigned to
+    var asignee = boardsAndPeople[board];
+    if(!asignee){
+      asignee = board;
     }
-                                                                                                                         
-    // if(boardAssignedTo == 'Project Management'){
-    //   taskassignedToBoard('Project Management1');
-    //   taskassignedToBoard('Project Management2');
-    // }
-    // else{
-      taskassignedToBoard(boardAssignedTo);
-    // }        
+    //underscores make it italics
+    var response = '_' + brief + '_ ' + 'assigned to ' + asignee + ' \nhttp://trello.com/c/'+ linky;
+    staffGroup.send(response);
   }
-  //card finished or updated
-  else if(actionType == 'updateCard'){
-      var destinationBoard = req.body.action.data.listAfter.name;
-      //card completed
-      if(destinationBoard == 'Done'){
-      var response = 'completed by '+TrelloNamesAndPeople[assigner]+': _' + brief + '_ ' + ' \nhttp://trello.com/c/'+ linky;
-      staffGroup.send(response);
-      }
-      //card updated
-      else{
-        var response = 'Trello card updated' + ' \nhttp://trello.com/c/'+ linky;
-      }
+  taskassignedToBoard(boardAssignedTo);
+}
+//card finished or updated
+else if(actionType == 'updateCard'){
+  var destinationBoard = req.body.action.data.listAfter.name;
+  //card completed
+  if(destinationBoard == 'Done'){
+    var response = 'completed by '+TrelloNamesAndPeople[assigner]+': _' + brief + '_ ' + ' \nhttp://trello.com/c/'+ linky;
+    staffGroup.send(response);
   }
+  //card updated
+  else{
+    var response = 'Trello card updated' + ' \nhttp://trello.com/c/'+ linky;
+  }
+}
   
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+console.log("Listening on " + port);
 });
