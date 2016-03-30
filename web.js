040d637e31197fbf44223fd84594f5ae312fd1b0
patxu <patrick.s.xu.17@dartmouth.edu>
2016-03-30 15:01:25 -0400

    qr code attempt 2

--- web.js
+++ web.js
@@ -17,6 +17,7 @@ var moment = require('moment-timezone');
 moment.tz.setDefault("America/New_York");
 var userDB = require('./user');
 var qr = require('qr-image');
+var QRCode = require('qrcode');
 
 console.log("dali hr-bot starting up");
 
@@ -169,11 +170,17 @@ var refreshAndAskHours = function() {
 };
 
 var sendQRCodes = function(res) {
-  console.log('generating qr code')
+  console.log('generating qr code1')
   var code = qr.image(new Date().toString(), { type: 'svg' });
   res.type('svg');
   code.pipe(res);
   console.log(res);
+
+  console.log('generating qr code2')
+  QRCode.toDataURL('i am a pony!',function(err,url){
+    console.log('printing qr code 2')
+    console.log(url);
+  });
 }
 
 //  when we first start refresh all slack stuff
