eb5cd839a8c6fcaceb0355dfeabc7f2ebfff33c5
patxu <patrick.s.xu.17@dartmouth.edu>
2016-04-08 01:23:12 -0400

    upload readme to user

--- web.js
+++ web.js
@@ -206,8 +206,8 @@ var sendQRCodes = function() {
 
     var form = r.form();
     form.append('token', token);
-    form.append('filename', 'qr-code.svg');
-    form.append('file', qr_string);
+    // form.append('file', qr_string);
+    form.append('file', './README.md');
     // form.append('channels', channel);
 
     console.log(form);
