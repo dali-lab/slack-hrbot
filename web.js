c9f3421558a434b88caf90089dd43f032299f8e1
patxu <patrick.s.xu.17@dartmouth.edu>
2016-04-09 21:29:54 -0400

    finding check in channel

--- web.js
+++ web.js
@@ -116,7 +116,7 @@ var refreshSlack = function() {
 
   // get checkInChannel
   checkInChannel = slack.getGroupByName('check-in');
-  console.log(slack.getUserByName('patxu'));
+  console.log(slack.getUserByName('patxu').real_name);
 
   var weekday = moment().day();
 
